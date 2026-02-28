310f5df0f1e1fd3d5f5a2a50fead6b0a39bd0e91
Saad Kabir Uddin <102184777+pro777oy@users.noreply.github.com>
2026-03-01 05:24:04 +0600

    update

--- axe_game.cpp
+++ axe_game.cpp
@@ -7,62 +7,87 @@ int main()
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Axe Game");
-
     SetTargetFPS(60);
 
+    // Player
 
-    int circleX = 175;
-    int circleY = 100;
+    const int startX = 400;
+    const int startY = 400;
+
+    int circleX = startX;
+    int circleY = startY;
+
     float radius = 50;
-    Color circleColor = MAROON;
+    Color circleColor = BLUE;
+    float circleSpeed = 600;
 
-
+    // Axe
     int axeX = 300;
     int axeY = 0;
     int axeWidth = 50;
     int axeHeight = 50;
-    float axeSpeed = 250;
+    float axeSpeed = 1000;
     Color axeColor = RED;
 
-    bool collisionWithAxe = false;
+    bool gameOver = false;
 
     while (!WindowShouldClose())
     {
         float deltaTime = GetFrameTime();
 
-        axeY += axeSpeed * deltaTime;
-
-        if (axeY > screenHeight)
+        if (!gameOver)
         {
-            axeY = -axeHeight;
-            axeX = GetRandomValue(0, screenWidth - axeWidth);
+            axeY += axeSpeed * deltaTime;
+
+            if (axeY > screenHeight)
+            {
+                axeY = -axeHeight;
+                axeX = GetRandomValue(0, screenWidth - axeWidth);
+            }
+
+            if (IsKeyDown(KEY_D) && circleX < screenWidth - radius)
+                circleX += circleSpeed * deltaTime;
+
+            if (IsKeyDown(KEY_A) && circleX > radius)
+                circleX -= circleSpeed * deltaTime;
+
+            if (IsKeyDown(KEY_W) && circleY > radius)
+                circleY -= circleSpeed * deltaTime;
+
+            if (IsKeyDown(KEY_S) && circleY < screenHeight - radius)
+                circleY += circleSpeed * deltaTime;
+
+            Rectangle axeRec = { (float)axeX, (float)axeY, (float)axeWidth, (float)axeHeight };
+            Rectangle circleRec = { circleX - radius, circleY - radius, radius * 2, radius * 2 };
+
+            if (CheckCollisionRecs(axeRec, circleRec))
+            {
+                gameOver = true;
+            }
+        }
+        else
+        {
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                circleX = startX;
+                circleY = startY;
+
+                axeY = 0;
+                axeX = GetRandomValue(0, screenWidth - axeWidth);
+
+                gameOver = false;
+            }
         }
 
- 
-        if(IsKeyDown(KEY_D) && circleX < screenWidth - radius)
-            circleX += 300 * deltaTime;
-
-        if(IsKeyDown(KEY_A) && circleX > radius)
-            circleX -= 300 * deltaTime;
-
-        if(IsKeyDown(KEY_W) && circleY > radius)
-            circleY -= 300 * deltaTime;
-
-        if(IsKeyDown(KEY_S) && circleY < screenHeight - radius)
-            circleY += 300 * deltaTime;
-
-
-        Rectangle axeRec = { (float)axeX, (float)axeY, (float)axeWidth, (float)axeHeight };
-        Rectangle circleRec = { circleX - radius, circleY - radius, radius * 2, radius * 2 };
-
-        collisionWithAxe = CheckCollisionRecs(axeRec, circleRec);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        if (collisionWithAxe)
+        if (gameOver)
         {
-            DrawText("Game Over!", screenWidth/2 - 100, screenHeight/2, 40, RED);
+            DrawText("GAME OVER!", screenWidth/2 - 150, screenHeight/2 - 40, 50, RED);
+            DrawText("Press ENTER to try again",screenWidth/2 - 130,screenHeight/2 + 20,20,DARKGRAY);
         }
         else
         {
